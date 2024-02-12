2115b4e174ff16e7b7f7526c670e00a28df10dc6
Vipul <vipuldamrepro26@gmail.com>
2024-02-12 23:53:57 +0530

    some changes has been done in chrome extension

--- Vipul_Damre/background.js
+++ Vipul_Damre/background.js
@@ -1,18 +1,19 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.incluedes('youtube.com/watch')) {
-
+    if (tab.url && tab.url.includes('youtube.com/watch')) {
+        // console.log('hey there');
+        // console.log(tab + "and" + tabId);
 
         // queryParameters is unique id for each video 
         const queryParameters = tab.url.split('?')[1];
-        console.log(queryParameters);
+        // console.log(queryParameters);
 
 
         // URLSearchParams() returns the object which have parameters of url for better useability without using direct url 
         const urlParameters = new URLSearchParams(queryParameters)
-        console.log(urlParameters);
+        // console.log(urlParameters);
 
         chrome.tabs.sendMessage(tabId, {
-            type: 'NEw',
+            type: 'NEW',
             videoId: urlParameters.get('v')
         })
     }
@@ -24,9 +25,9 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
 
 
 
-const queryParameters = 'https://www.youtube.com/watch?v=0n809nd4Zu4&ab_channel=freeCodeCamp.org'
-const urlParameters = new URLSearchParams(queryParameters)
-        console.log(urlParameters);
+// const queryParameters = 'https://www.youtube.com/watch?v=0n809nd4Zu4&ab_channel=freeCodeCamp.org'
+// const urlParameters = new URLSearchParams(queryParameters)
+//         console.log(urlParameters);
 
 
-console.log(urlParameters.get('v'));
+// console.log(urlParameters.get('v'));
--- Vipul_Damre/contentScript.js
+++ Vipul_Damre/contentScript.js
@@ -1,19 +1,106 @@
 (() => {
-    
-    let youtubeLeftControls, youtubePlayer;
+
+    let youtubeLeftControls, youtubePlayer;   
     let currentVideo = '';
+    currentVideoBookmarks = [];
 
-    // below line take a message comes from background.js of line 14
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
 
-        if(type === 'NEW'){
-            currentVideo = videoId;
-            newVideoLoaded();
+
+
+    // fetching bookmark data from storage 
+    const fetchBookmarks = () => {
+        return new Promise((resolve) => {
+          chrome.storage.sync.get([currentVideo], (obj) => {
+            resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+          });
+        });
+      };
+
+
+
+
+
+
+      const addNewBookmarkEventHandler = async () => {
+
+        // currentTime is a property to access video currentTime;
+        const currentTime = youtubePlayer.currentTime;
+        console.log(currentTime);
+
+        const newBookmark = {
+            time: currentTime,
+            desc: 'Bookmark at ' + getTime(currentTime),
         }
-    });
+        console.log(newBookmark);
 
+
+        currentVideoBookmarks = await fetchBookmarks();
+
+        
+
+        chrome.storage.sync.set({
+            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+        })
+        
+    }
+
+
+
+
+
+    const newVideoLoaded = async () => {
+        const bookmarkBtnExists = document.getElementsByClassName('bookmark-btn')[0];
+        // console.log(bookmarkBtnExists);
+
+        currentVideoBookmarks = await fetchBookmarks();
+
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement('img');
+
+            bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png');
+            bookmarkBtn.classList.add('bookmark-btn');
+            // bookmarkBtn.title = 'Click to bookmark current timestamp';
+
+
+            youtubeLeftControls = document.getElementsByClassName('ytp-left-controls')[0];
+            youtubePlayer = document.getElementsByClassName('video-stream')[0];
+            // console.log(youtubeLeftControls);
+            // console.log(youtubePlayer);
+
+            youtubeLeftControls.append(bookmarkBtn);
+            bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
+            
+        }
+    }
+
+
+
+      // below line take a message comes from background.js of line 14
+      chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
+    
+        if (type === "NEW") {
+          currentVideo = videoId;
+          newVideoLoaded();
+        } else if (type === "PLAY") {
+          youtubePlayer.currentTime = value;
+        } else if ( type === "DELETE") {
+          currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
+          chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) });
+    
+          response(currentVideoBookmarks);
+        }
+      });
+
+    newVideoLoaded();
 
 })();
 
 
+
+const getTime = t => {
+    var date = new Date(0);
+    date.setSeconds(t);
+  
+    return date.toISOString().substr(11, 8);
+  };
